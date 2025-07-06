90170bf2c580d4c09865c6490a67568431c61c76
Poulami Gandhi <104186083+Poulami-07@users.noreply.github.com>
2025-07-06 15:34:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://ink-swift-server.vercel.app/',
         changeOrigin: true,
         secure: false,
       },
